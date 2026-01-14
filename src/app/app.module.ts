e9c6ec7112e126754adb743751bd4a3db2c21e62
Madhavan K R <madhavan.kalkunte@gmail.com>
2026-01-13 21:06:13 -0500

    update to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatRadioModule} from '@angular/material/radio';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
@@ -30,39 +30,32 @@ import { OAuthModule } from 'angular-oauth2-oidc';
 import { CallbackComponentComponent } from './components/callback-component/callback-component.component';
 import { ProfilePersonaComponentComponent } from './components/profile-persona-component/profile-persona-component.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    MainLayoutComponent,
-    EnhancedKeyboardComponent,
-    HoverClickDirective,
-    MainPageV2Component,
-    ProfileFormComponent,
-    CallbackComponentComponent,
-    ProfilePersonaComponentComponent,
-  ],
-  imports: [
-    RouterModule,
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatGridListModule,
-    MatButtonModule,
-    HttpClientModule,
-    MatInputModule,
-    MatFormFieldModule,
-    FormsModule,
-    MatChipsModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatDialogModule,
-    MatProgressSpinnerModule,
-    OAuthModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        MainLayoutComponent,
+        EnhancedKeyboardComponent,
+        HoverClickDirective,
+        MainPageV2Component,
+        ProfileFormComponent,
+        CallbackComponentComponent,
+        ProfilePersonaComponentComponent,
+    ],
+    bootstrap: [AppComponent], imports: [RouterModule,
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatGridListModule,
+        MatButtonModule,
+        MatInputModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatChipsModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatDialogModule,
+        MatProgressSpinnerModule,
+        OAuthModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
